b7f8e62fe733f5120d7cafe116a8e5f56f03605f
lRozx <1010137068@u.icesi.edu.co>
2023-04-05 19:27:12 -0500

    Fixed

--- pages/index.tsx
+++ pages/index.tsx
@@ -115,7 +115,7 @@ const Home: NextPage = () => {
             <>
               <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
                 {generatedBios
-                  .substring(generatedBios.indexOf("1") + 3)
+                  .substring(generatedBios.indexOf("1"))
                   .split("____")
                   .map((generatedBio) => {
                     return (
