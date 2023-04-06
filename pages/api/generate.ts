db9391de603e5397a0e65f08ea1da98d19fc692f
Santiago Orozco <san7yz@gmail.com>
2023-04-06 02:25:49 +0000

    Added System Prompt

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -19,28 +19,27 @@ const handler = async (req: Request): Promise<Response> => {
     return new Response("No prompt in the request", { status: 400 });
   }
 
-  try {
-    const response = await axios.get(prompt);
-    const $ = cheerio.load(response.data);
-    const text = $("body").text().trim();
+  const payload: OpenAIStreamPayload = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "user",
+        content:
+          "Generate a humorous and entertaining roas of the text of a website that I provide, a roast that will make me laugh out loud!. No more than 400 characters. Give me 1 recommendation to improve the website",
+      },
+      { role: "user", content: prompt },
+    ],
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 200,
+    stream: true,
+    n: 1,
+  };
 
-    const payload: OpenAIStreamPayload = {
-      model: "gpt-3.5-turbo",
-      messages: [{ role: "user", content: text }],
-      temperature: 0.7,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      max_tokens: 200,
-      stream: true,
-      n: 1,
-    };
-
-    const stream = await OpenAIStream(payload);
-    return new Response(stream);
-  } catch (error) {
-    return new Response("Error getting text from URL", { status: 500 });
-  }
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
 };
 
 export default handler;
