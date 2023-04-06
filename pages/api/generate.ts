c52b16380612e765188e06f89717e430a4b8d7dc
Santiago Orozco <san7yz@gmail.com>
2023-04-06 15:41:36 +0000

    Integrate scraper

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,6 +1,4 @@
 import { OpenAIStream, OpenAIStreamPayload } from "../../utils/OpenAIStream";
-import axios from "axios";
-import cheerio from "cheerio";
 
 if (!process.env.OPENAI_API_KEY) {
   throw new Error("Missing env var from OpenAI");
@@ -19,37 +17,31 @@ const handler = async (req: Request): Promise<Response> => {
     return new Response("No prompt in the request", { status: 400 });
   }
 
-  try {
-    // Scrape the website
-    const response = await axios.get(prompt);
-    const html = response.data;
-    const $ = cheerio.load(html);
-    const text = $("body").text().replace(/\n/g, " ").substring(0, 400);
+  // call Scraper API to get website text
+  const scraperResponse = await fetch(`/api/scraper?url=${prompt}`);
+  const { text } = await scraperResponse.json();
 
-    const payload: OpenAIStreamPayload = {
-      model: "gpt-3.5-turbo",
-      messages: [
-        {
-          role: "system",
-          content:
-            "Generate a humorous and entertaining roast of the text of a website that I provide, a roast that will make me laugh out loud!. No more than 400 characters. Give me 1 recommendation to improve the website",
-        },
-        { role: "user", content: text },
-      ],
-      temperature: 0.7,
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      max_tokens: 200,
-      stream: true,
-      n: 1,
-    };
+  const payload: OpenAIStreamPayload = {
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "Generate a humorous and entertaining roast of the text of a website that I provide, a roast that will make me laugh out loud!. No more than 400 characters. Give me 1 recommendation to improve the website",
+      },
+      { role: "user", content: text },
+    ],
+    temperature: 0.7,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 200,
+    stream: true,
+    n: 1,
+  };
 
-    const stream = await OpenAIStream(payload);
-    return new Response(stream);
-  } catch (error) {
-    return new Response("Error generating content", { status: 500 });
-  }
+  const stream = await OpenAIStream(payload);
+  return new Response(stream);
 };
 
 export default handler;
