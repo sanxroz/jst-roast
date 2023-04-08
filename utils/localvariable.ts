60937b8853de52ec3002cdca3032108e38d212df
Santiago Orozco <san7yz@gmail.com>
2023-04-08 03:52:08 +0000

    Update it

--- utils/localvariable.ts
+++
@@ -1,7 +0,0 @@
-export function getInputValue() {
-  if (typeof window !== "undefined") {
-    const inputValue = localStorage.getItem("inputValue");
-    return inputValue ? JSON.parse(inputValue) : null;
-  }
-  return null;
-}
